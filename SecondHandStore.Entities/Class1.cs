862116b1b299cca155e398996c941a38acca9b4a
Marija Todorova <marija283@hotmail.com>
2016-08-14 15:21:37 +0200

    Created new project

---
+++ SecondHandStore.Entities/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondHandStore.Entities
+{
+    public class Class1
+    { 
+        hello
+    }
+}
---
+++ SecondHandStoreApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SecondHandStoreApp.Startup))]
+namespace SecondHandStoreApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
