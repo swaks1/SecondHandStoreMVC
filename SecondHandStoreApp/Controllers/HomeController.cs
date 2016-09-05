56db7715a073cf6ad2092de090beda291e924646
Riste Poposki <rickyarpy@hotmail.com>
2016-09-05 16:05:11 +0200

    payment ok and mail

--- SecondHandStoreApp/Controllers/HomeController.cs
+++ SecondHandStoreApp/Controllers/HomeController.cs
@@ -9,10 +9,10 @@ using PagedList;
 using System.Threading.Tasks;
 using SendGrid;
 using SendGrid.Helpers.Mail;
-using PayPal.Api;
+using Microsoft.AspNet.Identity;
+using System.Net.Mail;
+using System.Net.Mime;
 using System.Net;
-using System.Text;
-using System.IO;
 
 namespace SecondHandStoreApp.Controllers
 {
@@ -359,323 +359,55 @@ namespace SecondHandStoreApp.Controllers
 
         static async Task SendContactMailTask(ContactViewModel model)
         {
-            string apiKey = System.Configuration.ConfigurationManager.AppSettings["mailApiKey"];
-            dynamic sg = new SendGridAPIClient(apiKey);
+            //string apiKey = System.Configuration.ConfigurationManager.AppSettings["mailApiKey"];
+            //dynamic sg = new SendGridAPIClient(apiKey);
 
-            Email to = new Email("marija283@hotmail.com");// company address
-            Email from = new Email(model.Email, model.FirstName + " " + model.LastName );
-            string subject = model.Subject;
-            Content content = new Content("text/plain", model.Message);
-            Mail mail = new Mail(from, subject, to, content);
+            //Email to = new Email("marija283@hotmail.com");// company address
+            //Email from = new Email(model.Email, model.FirstName + " " + model.LastName );
+            //string subject = model.Subject;
+            //Content content = new Content("text/plain", model.Message);
+            //Mail mail = new Mail(from, subject, to, content);
 
-            dynamic response = await sg.client.mail.send.post(requestBody: mail.Get());
-        }
-        
-        public ActionResult PaymentWithCreditCard()
-        {
-            //create and item for which you are taking payment
-            //if you need to add more items in the list
-            //Then you will need to create multiple item objects or use some loop to instantiate object
-            Item item = new Item();
-            item.name = "Demo Item";
-            item.currency = "USD";
-            item.price = "5";
-            item.quantity = "1";
-            item.sku = "sku";
-
-            //Now make a List of Item and add the above item to it
-            //you can create as many items as you want and add to this list
-            List<Item> itms = new List<Item>();
-            itms.Add(item);
-            ItemList itemList = new ItemList();
-            itemList.items = itms;
-
-            //Address for the payment
-            Address billingAddress = new Address();
-            billingAddress.city = "NewYork";
-            billingAddress.country_code = "US";
-            billingAddress.line1 = "23rd street kew gardens";
-            billingAddress.postal_code = "43210";
-            billingAddress.state = "NY";
-
-
-            //Now Create an object of credit card and add above details to it
-            //Please replace your credit card details over here which you got from paypal
-            CreditCard crdtCard = new CreditCard();
-            crdtCard.billing_address = billingAddress;
-            crdtCard.cvv2 = "874";  //card cvv2 number
-            crdtCard.expire_month = 10; //card expire date
-            crdtCard.expire_year = 2021; //card expire year
-            crdtCard.first_name = "Marija";
-            crdtCard.last_name = "Todorova";
-            crdtCard.number = "4032036140345909"; //enter your credit card number here
-            crdtCard.type = "visa"; //credit card type here paypal allows 4 types
-
-            // Specify details of your payment amount.
-            Details details = new Details();
-            details.shipping = "1";
-            details.subtotal = "5";
-            details.tax = "1";
-
-            // Specify your total payment amount and assign the details object
-            Amount amnt = new Amount();
-            amnt.currency = "USD";
-            // Total = shipping tax + subtotal.
-            amnt.total = "7";
-            amnt.details = details;
-
-            // Now make a transaction object and assign the Amount object
-            Transaction tran = new Transaction();
-            tran.amount = amnt;
-            tran.description = "Description about the payment amount.";
-            tran.item_list = itemList;
-            tran.invoice_number = "your invoice number which you are generating";
-
-            // Now, we have to make a list of transaction and add the transactions object
-            // to this list. You can create one or more object as per your requirements
-
-            List<Transaction> transactions = new List<Transaction>();
-            transactions.Add(tran);
-
-            // Now we need to specify the FundingInstrument of the Payer
-            // for credit card payments, set the CreditCard which we made above
-
-            FundingInstrument fundInstrument = new FundingInstrument();
-            fundInstrument.credit_card = crdtCard;
-
-            // The Payment creation API requires a list of FundingIntrument
-
-            List<FundingInstrument> fundingInstrumentList = new List<FundingInstrument>();
-            fundingInstrumentList.Add(fundInstrument);
-
-            // Now create Payer object and assign the fundinginstrument list to the object
-            Payer payr = new Payer();
-            payr.funding_instruments = fundingInstrumentList;
-            payr.payment_method = "credit_card";
-
-            // finally create the payment object and assign the payer object & transaction list to it
-            Payment pymnt = new Payment();
-            pymnt.intent = "sale";
-            pymnt.payer = payr;
-            pymnt.transactions = transactions;
-
-            
-                //getting context from the paypal
-                //basically we are sending the clientID and clientSecret key in this function
-                //to the get the context from the paypal API to make the payment
-                //for which we have created the object above.
-
-                //Basically, apiContext object has a accesstoken which is sent by the paypal
-                //to authenticate the payment to facilitator account.
-                //An access token could be an alphanumeric string
-
-                APIContext apiContext = Configuration.GetAPIContext();
-
-                //Create is a Payment class function which actually sends the payment details
-                //to the paypal API for the payment. The function is passed with the ApiContext
-                //which we received above.
-
-                Payment createdPayment = pymnt.Create(apiContext);
-
-                //if the createdPayment.state is "approved" it means the payment was successful else not
-
-                if (createdPayment.state.ToLower() != "approved")
-                {
-                    return Json("NOT OK", JsonRequestBehavior.AllowGet);
-                }
-            
-            
-
-            return Json("OK", JsonRequestBehavior.AllowGet);
-        }
-
-
-        public ActionResult PaymentWithPaypal()
-        {
-            //getting the apiContext as earlier
-            APIContext apiContext = Configuration.GetAPIContext();
-
-            try
+            //dynamic response = await sg.client.mail.send.post(requestBody: mail.Get());
+            IdentityMessage message = new IdentityMessage
             {
-                string payerId = Request.Params["PayerID"];
+                Body = model.Message,
+                Subject = model.Subject,
+                Destination = model.Email
 
-                if (string.IsNullOrEmpty(payerId))
-                {
-                    //this section will be executed first because PayerID doesn't exist
-                    //it is returned by the create function call of the payment class
-
-                    // Creating a payment
-                    // baseURL is the url on which paypal sendsback the data.
-                    // So we have provided URL of this controller only
-                    string baseURI = Request.Url.Scheme + "://" + Request.Url.Authority +
-                                "/Home/PaymentWithPayPal?";
-
-                    //guid we are generating for storing the paymentID received in session
-                    //after calling the create function and it is used in the payment execution
-
-                    var guid = Convert.ToString((new Random()).Next(100000));
-
-                    //CreatePayment function gives us the payment approval url
-                    //on which payer is redirected for paypal account payment
-
-                    var createdPayment = this.CreatePayment(apiContext, baseURI + "guid=" + guid);
-
-                    //get links returned from paypal in response to Create function call
-
-                    var links = createdPayment.links.GetEnumerator();
-
-                    string paypalRedirectUrl = null;
-
-                    while (links.MoveNext())
-                    {
-                        Links lnk = links.Current;
-
-                        if (lnk.rel.ToLower().Trim().Equals("approval_url"))
-                        {
-                            //saving the payapalredirect URL to which user will be redirected for payment
-                            paypalRedirectUrl = lnk.href;
-                        }
-                    }
-
-                    // saving the paymentID in the key guid
-                    Session.Add(guid, createdPayment.id);
-
-                    return Redirect(paypalRedirectUrl);
-                }
-                else
-                {
-                    // This section is executed when we have received all the payments parameters
-
-                    // from the previous call to the function Create
-
-                    // Executing a payment
-
-                    var guid = Request.Params["guid"];
-
-                    var executedPayment = ExecutePayment(apiContext, payerId, Session[guid] as string);
-
-                    if (executedPayment.state.ToLower() != "approved")
-                    {
-                        return View("FailureView");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-              
-                return Json("NOT OK",JsonRequestBehavior.AllowGet);
-            }
-
-            return Json("OK", JsonRequestBehavior.AllowGet);
-        }
-
-        private Payment payment;
-
-        private Payment ExecutePayment(APIContext apiContext, string payerId, string paymentId)
-        {
-            var paymentExecution = new PaymentExecution() { payer_id = payerId };
-            this.payment = new Payment() { id = paymentId };
-            return this.payment.Execute(apiContext, paymentExecution);
-        }
-
-        private Payment CreatePayment(APIContext apiContext, string redirectUrl)
-        {
-
-            //similar to credit card create itemlist and add item objects to it
-            var itemList = new ItemList() { items = new List<Item>() };
-
-            itemList.items.Add(new Item()
-            {
-                name = "Item Name",
-                currency = "USD",
-                price = "5",
-                quantity = "1",
-                sku = "sku"
-            });
-
-            var payer = new Payer() { payment_method = "paypal" };
-
-            // Configure Redirect Urls here with RedirectUrls object
-            var redirUrls = new RedirectUrls()
-            {
-                cancel_url = redirectUrl,
-                return_url = redirectUrl
             };
-
-            // similar as we did for credit card, do here and create details object
-            var details = new Details()
-            {
-                tax = "1",
-                shipping = "1",
-                subtotal = "5"
-            };
-
-            // similar as we did for credit card, do here and create amount object
-            var amount = new Amount()
-            {
-                currency = "USD",
-                total = "7", // Total must be equal to sum of shipping, tax and subtotal.
-                details = details
-            };
-
-            var transactionList = new List<Transaction>();
-
-            transactionList.Add(new Transaction()
-            {
-                description = "Transaction description.",
-                invoice_number = "your invoice number",
-                amount = amount,
-                item_list = itemList
-            });
-
-            this.payment = new Payment()
-            {
-                intent = "sale",
-                payer = payer,
-                transactions = transactionList,
-                redirect_urls = redirUrls
-            };
-
-            // Create a payment using a APIContext
-            return this.payment.Create(apiContext);
+            sendMail(message);
         }
 
-        internal static class Configuration
+
+        static void sendMail(IdentityMessage message)
         {
-            //these variables will store the clientID and clientSecret
-            //by reading them from the web.config
-            public readonly static string ClientId;
-            public readonly static string ClientSecret;
+            #region formatter
+            string text = string.Format(message.Body);
+            string html = "<strong>" + message.Body + "</strong>";
 
-            static Configuration()
+            #endregion
+
+            MailMessage msg = new MailMessage();
+            msg.From = new MailAddress("SecondHandStore@EMT.com");
+            msg.To.Add(new MailAddress(message.Destination));
+            msg.Subject = message.Subject;
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+            msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+            using (SmtpClient smtpClient = new SmtpClient())
             {
-                var config = GetConfig();
-                ClientId = config["clientId"];
-                ClientSecret = config["clientSecret"];
+                NetworkCredential credentials = new NetworkCredential("secondhandstoreemt@gmail.com", "secondstore123");
+                smtpClient.Credentials = credentials;
+                smtpClient.Host = "smtp.gmail.com";
+                smtpClient.Port = 587;
+                smtpClient.EnableSsl = true;
+                smtpClient.Send(msg);
+
             }
 
-            // getting properties from the web.config
-            public static Dictionary<string, string> GetConfig()
-            {
-                return ConfigManager.Instance.GetProperties();
-            }
-
-            private static string GetAccessToken()
-            {
-                // getting accesstocken from paypal                
-                string accessToken = new OAuthTokenCredential
-            (ClientId, ClientSecret, GetConfig()).GetAccessToken();
-
-                return accessToken;
-            }
-
-            public static APIContext GetAPIContext()
-            {
-                // return apicontext object by invoking it with the accesstoken
-                APIContext apiContext = new APIContext(GetAccessToken());
-                apiContext.Config = GetConfig();
-                return apiContext;
-            }
         }
+
+
     }
 }
